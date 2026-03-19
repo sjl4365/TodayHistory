adc5e9d949c2751eff7a09fdc4a65324a01c4e50
Kwanyeob <kyeobj@gmail.com>
2026-03-19 19:59:50 -0400

    version ++

--- app/(tabs)/settings/setting.js
+++ app/(tabs)/settings/setting.js
@@ -319,7 +319,7 @@ export default function SettingsIndex() {
           <SettingItem
             title={t('appVersion')}
             rightComponent={
-              <Text style={[styles.selectedLanguageText, { fontSize: scale(16) }]}>v 1.0.13</Text>
+              <Text style={[styles.selectedLanguageText, { fontSize: scale(16) }]}>v 1.0.14</Text>
             }
             showArrow={false}
           />
