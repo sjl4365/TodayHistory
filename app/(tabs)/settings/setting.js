252ecea305aa6b1a766317ffc043b4be24d5ee95
ktyeon <qazppo@naver.com>
2026-03-24 19:43:58 -0400

    update version

--- app/(tabs)/settings/setting.js
+++ app/(tabs)/settings/setting.js
@@ -310,7 +310,7 @@ export default function SettingsIndex() {
           <SettingItem
             title={t('appVersion')}
             rightComponent={
-              <Text style={[styles.selectedLanguageText, { fontSize: scale(16) }]}>v 1.0.16</Text>
+              <Text style={[styles.selectedLanguageText, { fontSize: scale(16) }]}>v 1.0.17</Text>
             }
             showArrow={false}
           />
